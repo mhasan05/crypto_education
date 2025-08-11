c473ee34a132bac807e1e50ea7cd26c8e2dba39c
Md Mehedi Hasan <mhasan.dev2021@gmail.com>
2025-08-11 09:50:34 +0600

    update

--- core/settings.py
+++ core/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
-MEDIA_URLS ='/media/'
+MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
